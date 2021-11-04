297faa6859cac530573b9cf81217a109ba5198c5
VitorRezzo <cv.rezz99@gmail.com>
2021-11-04 17:52:46 -0300

    botão drop lateral pagina home

--- src/pages/Home/components/MenuListIU.js
+++ src/pages/Home/components/MenuListIU.js
@@ -14,9 +14,14 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import MenuItem from "@material-ui/core/MenuItem";
 import Popover from "@material-ui/core/Popover";
+import { makeStyles } from "@material-ui/styles";
 
 export const ListMenu = () => {
   const [anchorEl, setAnchorEl] = useState(null);
+  const [anchorUser, setAnchorUser] = useState(null);
+
+  const open = Boolean(anchorEl);
+  const openuser = Boolean(anchorUser);
 
   const handleOpen = (event) => {
     setAnchorEl(event.currentTarget);
@@ -24,42 +29,68 @@ export const ListMenu = () => {
 
   const handleClose = () => {
     setAnchorEl(null);
+    setAnchorUser(null);
   };
 
-  const open = Boolean(anchorEl);
+  const useStyles = makeStyles((theme: Theme) => ({
+    menu: {
+      "& .MuiPaper-root": {
+        background: "linear-gradient(90deg, #F28705 35%, #F2B705 60% )",
+        borderRadius: 2,
+      },
+    },
+  }));
+
+  const classes = useStyles();
 
   return (
     <List sx={{ height: "90vh" }}>
-      <ListItem button>
+      <ListItem button={true}>
         <ListItemIcon>
           <DashboardIcon />
         </ListItemIcon>
         <ListItemText primary="Dashboard" />
       </ListItem>
 
-      <ListItem button onClick={() => window.electron.openuserCadWindow()}>
+      <ListItem
+        onMouseEnter={(event) => setAnchorUser(event.currentTarget)}
+        button
+      >
         <ListItemIcon>
           <PeopleIcon />
         </ListItemIcon>
         <ListItemText primary="Usuarios" />
+
+        <Popover
+          className={classes.menu}
+          anchorEl={anchorUser}
+          elevation={0}
+          open={openuser}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={() => window.electron.openuserCadWindow()}>
+            Cadastro
+          </MenuItem>
+        </Popover>
       </ListItem>
 
-      <ListItem
-        sx={{ WebkitAppRegion: "no-drag" }}
-        onMouseEnter={handleOpen}
-        button
-      >
+      <ListItem onMouseEnter={handleOpen} button>
         <ListItemIcon>
           <ThumbsUpDownIcon />
         </ListItemIcon>
         <ListItemText primary="Atendimentos" />
         <Popover
-          onMouseEnter={handleOpen}
-          onMouseLeave={handleClose}
-          sx={{
-            pointerEvents: "none",
-          }}
+          className={classes.menu}
           anchorEl={anchorEl}
+          elevation={0}
           open={open}
           anchorOrigin={{
             vertical: "top",
@@ -70,7 +101,6 @@ export const ListMenu = () => {
             horizontal: "left",
           }}
           onClose={handleClose}
-          disableFocus
         >
           <MenuItem onClick={() => window.electron.opensuporteWindow()}>
             Suporte
